4c656359622b94914becc54b674664c7c4026897
David Ball <davidball117@gmail.com>
2021-12-05 07:39:58 -0500

    Added husky packages.

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,5 @@
-console.log(
+console.log('app.ts TESTING 1');
 
+//   kadjflhsfghas
 
-
-
-         'app.ts TESTING 1' 
-    )  
- ;
-
-
-
-
-
-
-     //   kadjflhsfghas
-
-     
-console.log   (
-               'app.ts TESTING 2'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-)
-
-
-
-
+console.log('app.ts TESTING 2');
